759d2c1b5267bd3812b28aaa5dbb79e4d80eec00
Tamar Almogy <tamar.almogy@post.runi.ac.il>
2024-06-05 12:59:31 +0300

    updated URL

--- server.js
+++ server.js
@@ -149,5 +149,8 @@ io.on("connection", (socket) => {
   });
 });
 
-const PORT = process.env.PORT || 3001;
+const PORT =
+  process.env.PORT ||
+  3001 ||
+  "https://coding-app-server-production.up.railway.app";
 server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
